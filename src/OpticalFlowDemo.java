83977952b3d28b1b526615a3554b870fbdc754cb
yzhang0127 <115376142+yzhang0127@users.noreply.github.com>
2023-07-20 20:19:41 -0400

    Delete src directory

--- src/OpticalFlowDemo.java
+++
@@ -1,8 +0,0 @@
-import org.opencv.core.Core;
-
-public class OpticalFlowDemo {
-    public static void main(String[] args) {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        new OptFlow().run(/*args*/);
-    }
-}
--- src/OpticalFlowDenseDemo.java
+++
@@ -1,8 +0,0 @@
-import org.opencv.core.Core;
-
-public class OpticalFlowDenseDemo {
-    public static void main(String[] args) {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        new OpticalDense().run(/*args*/);
-    }
-}
