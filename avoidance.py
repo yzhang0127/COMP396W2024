00bb67468c46cb9bd1bbb22864aeb6cdaf9f0860
Yijun Zhang <yijun.zhang@mail.mcgill.ca>
2024-05-19 14:03:50 -0400

    Final version for aircraft

--- avoidance.py
+++ avoidance.py
@@ -34,7 +34,7 @@ def load_model(model_type):
         transform = midas_transforms.small_transform
     return transform,midas
 
-def estimation(dimX, dimY, modelName,cam_channel=0,mode="ground"):
+def estimation(dimX, dimY, modelName,cam_channel=0,mode="air"):
 
     '''
 
@@ -58,7 +58,7 @@ def estimation(dimX, dimY, modelName,cam_channel=0,mode="ground"):
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
 
 
-
+    last_center = None
     while cap.isOpened():
 
         start = time.time()
@@ -89,18 +89,24 @@ def estimation(dimX, dimY, modelName,cam_channel=0,mode="ground"):
             #best_plane = utils.findNorms(output_norm,mid_height,mid_width,frame)
             t1.start()
 
-            safe_region_size = 4
-            visited_safe = []
-            visited_unsafe = []
-            tried_center = []
-            safe_center_count = []
-            best_center = []
-            candidate_center = None
-            min_rand = 4
-            max_rand = 8 - safe_region_size + 1
+        safe_region_size = 4
+        visited_safe = []
+        visited_unsafe = []
+        tried_center = []
+        safe_center_count = []
+        best_center = []
+        candidate_center = None
+        min_rand = 4
+        max_rand = 8 - safe_region_size + 1
+        skip = False
 
+        if last_center != None:
+            if np.mean(last_center) <= 0.02:
+                candidate_center = last_center
+                skip = True
+
+        if not skip:
             while len(tried_center)< pow((max_rand-min_rand+1),2):
-
                 queue = []
                 visited = []
                 c_x = 4
@@ -124,34 +130,42 @@ def estimation(dimX, dimY, modelName,cam_channel=0,mode="ground"):
                     elif coord not in visited_safe:
                         subMat = output_norm[y * height_interval:height_interval + y * height_interval,
                                  x * width_interval:width_interval + x * width_interval]
-
                         subMat = [[0.01 if a <= 0.5 else a for a in row] for row in subMat]
-                        # filter out ground
-                        if y >= 5:
-                            t1.join()
-                            best_plane = results[0]
+                        if mode == "ground":
+                            # filter out ground
+                            if y >= 5:
+                                t1.join()
+                                best_plane = results[0]
 
-                            for i in range(0, len(subMat)):
-                                for j in range(0, len(subMat)):
-                                    r_x = j + x * width_interval
-                                    r_y = i + y * height_interval
-                                    d = best_plane[0][0] * r_x + best_plane[0][1] * r_y + best_plane[0][2] * \
-                                        output_norm[r_y][r_x]
-                                    if abs(abs(d) - abs(best_plane[1])) <= 20.0:
-                                        subMat[i][j] = 0.01
-                            if np.mean(subMat) < 0.6:
-                                # safe
-                                visited_safe.append(coord)
-                                safe_count+=1
+                                for i in range(0, len(subMat)):
+                                    for j in range(0, len(subMat)):
+                                        r_x = j + x * width_interval
+                                        r_y = i + y * height_interval
+                                        d = best_plane[0][0] * r_x + best_plane[0][1] * r_y + best_plane[0][2] * \
+                                            output_norm[r_y][r_x]
+                                        if abs(abs(d) - abs(best_plane[1])) <= 20.0:
+                                            subMat[i][j] = 0.01
+                                if np.mean(subMat) < 0.6:
+                                    # safe
+                                    visited_safe.append(coord)
+                                    safe_count+=1
+                                else:
+                                    visited_unsafe.append(coord)
+                                    break
                             else:
-                                visited_unsafe.append(coord)
-                                break
+                                # print("ground: ",np.mean(subMat))
+                                if np.mean(subMat) <= 0.02:
+                                    # safe
+                                    visited_safe.append(coord)
+                                    safe_count+=1
+                                else:
+                                    visited_unsafe.append(coord)
+                                    break
                         else:
-                            # print("ground: ",np.mean(subMat))
                             if np.mean(subMat) <= 0.02:
                                 # safe
                                 visited_safe.append(coord)
-                                safe_count+=1
+                                safe_count += 1
                             else:
                                 visited_unsafe.append(coord)
                                 break
@@ -182,24 +196,29 @@ def estimation(dimX, dimY, modelName,cam_channel=0,mode="ground"):
             else:
                 candidate_center = best_center
 
-            if candidate_center!=None:
-                sX = (candidate_center[0]-safe_region_size+1)*width_interval
-                sY = (candidate_center[1]-safe_region_size+1)*height_interval
-                eX = (candidate_center[0]+safe_region_size)*width_interval
-                eY = (candidate_center[1]+safe_region_size)*height_interval
-                cv2.rectangle(frame,(sX,sY),(eX,eY),(255,0,0),2)
+
+        if candidate_center!=None:
+            sX = (candidate_center[0]-safe_region_size+1)*width_interval
+            sY = (candidate_center[1]-safe_region_size+1)*height_interval
+            eX = (candidate_center[0]+safe_region_size)*width_interval
+            eY = (candidate_center[1]+safe_region_size)*height_interval
+            cv2.rectangle(frame,(sX,sY),(eX,eY),(255,0,0),2)
+            last_center = candidate_center
+        else:
+            left = output_norm[0:frame.shape[1],0:mid_width]
+            right = output_norm[0:frame.shape[1],mid_width:frame.shape[0]]
+            meanL = np.mean(left)
+            meanR = np.mean(right)
+
+            if meanR>=0.6 and meanL>=0.6:
+                print("STOP")
             else:
-                left = output_norm[0:frame.shape[1],0:mid_width]
-                right = output_norm[0:frame.shape[1],mid_width:frame.shape[0]]
-                meanL = np.mean(left)
-                meanR = np.mean(right)
-                if meanR>=0.6 and meanL>=0.6:
-                    print("STOP")
+                if meanL  > meanR:
+                    cv2.rectangle(frame, (0, 0), (mid_width, frame.shape[0]),(0,255,255),2)
+                elif meanL < meanR:
+                    cv2.rectangle(frame,(mid_width,0),(frame.shape[1],frame.shape[0]),(0,255,255),2)
                 else:
-                    if meanL - meanR >= 0.1:
-                        cv2.rectangle(frame,(mid_width,0),(frame.shape[1],frame.shape[0]),(0,255,255),2)
-                    else:
-                        cv2.rectangle(frame, (0, 0), (mid_width, frame.shape[0]),(0,255,255),2)
+                    print("STOP")
 
 
             '''
